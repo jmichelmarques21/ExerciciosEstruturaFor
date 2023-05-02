40a7f10d306037ed4d63f70bd987c4080d7ee560
Jean Marques <jmichelmarques21@outlook.com>
2023-05-02 01:06:54 -0300

    a

--- ExerciciosEstruturaFor/Program.cs
+++ ExerciciosEstruturaFor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExerciciosEstruturaFor {
     class Program {
@@ -38,6 +39,29 @@ namespace ExerciciosEstruturaFor {
 
             Console.WriteLine("In: " + dentro);
             Console.WriteLine("Out: " + fora); */
+
+
+            /* Exercício 03 - Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+            de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+            conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+            peso 5.
+
+
+            Console.Write("Olá, quantos testes serão realizados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+                double media = (a * 2 + b * 3 + c * 5) / 10;
+
+                Console.WriteLine("Média: " + media.ToString("F1", CultureInfo.InvariantCulture));
+            } */ 
+
 
 
         }
